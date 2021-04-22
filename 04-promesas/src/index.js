b00192bafd8761baeefee5d896ad66a34e1ab908
CesarArellano <raywayday@gmail.com>
2021-04-22 17:00:25 -0500

    Await

--- 04-promesas/src/index.js
+++ 04-promesas/src/index.js
@@ -1,1 +1,17 @@
-import './styles.css';
+// import {promesaLenta, promesaMedia, promesaRapida } from './js/promesas'
+import { obtenerHeroesArr } from './js/await';
+
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+// Promise.race([promesaLenta, promesaMedia, promesaRapida])
+//   .then(  console.log  )
+//   .catch( console.warn );
+
+// buscarHeroe('capi').then( console.log );
+// buscarHeroeAsync('capi2')
+//   .then( console.log )
+//   .catch( console.warn );
+
+const heroes = obtenerHeroesArr().then(console.table);
--- 04-promesas/src/js/promesas.js
+++ 04-promesas/src/js/promesas.js
@@ -13,7 +13,8 @@ const heroes = {
   }
 };
 
-export const buscarHeroe = ( id ) => {
+// Promesas
+const buscarHeroe = ( id ) => {
   const heroe = heroes[id];
   return new Promise( (resolve, reject) => {
     if(heroe) {
@@ -22,4 +23,36 @@ export const buscarHeroe = ( id ) => {
       reject(`No existe un héroe con el id ${id}`);
     }
   });
+}
+
+// Async
+const buscarHeroeAsync = async ( id ) => {
+  const heroe = heroes[id];
+
+  if(heroe) {
+    return heroe;
+  } else {
+    throw Error(`No existe un héroe con el id ${id}`); // Si no se ocupa Error()
+  }
+}
+
+
+const promesaLenta = new Promise( ( res, rej ) => {
+  setTimeout(() => res('Promesa Lenta'), 2000);
+});
+
+const promesaMedia = new Promise( ( res, rej ) => {
+  setTimeout(() => res('Promesa Media'), 1500); // No importa si da reject porque la promesa rápida es más rápida xd.
+});
+
+const promesaRapida = new Promise( ( res, rej ) => {
+  setTimeout(() => res('Promesa Rápida'), 1000);
+});
+
+export {
+  promesaLenta,
+  promesaMedia,
+  promesaRapida,
+  buscarHeroe,
+  buscarHeroeAsync
 }
