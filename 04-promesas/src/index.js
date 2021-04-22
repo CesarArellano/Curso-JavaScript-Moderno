03c02a1a9e76db21bb6cea75d5aa7940b101cb33
CesarArellano <raywayday@gmail.com>
2021-04-22 11:51:18 -0500

    callback hell

--- 04-promesas/src/index.js
+++ 04-promesas/src/index.js
@@ -1,8 +1,21 @@
 import './styles.css';
 import { buscarHeroe } from './js/callbacks';
 
-const heroeId = 'capi';
+// const heroeId = 'capi';
 
-buscarHeroe( heroeId, (err, heroe) => {
-  (err) ? console.error(err) : console.log(heroe);
+// buscarHeroe( heroeId, (err, heroe) => {
+//   (err) ? console.error(err) : console.log(heroe);
+// });
+
+// Callback Hell, el código se vuelve un desastre.
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
+
+buscarHeroe( heroeId1, (err, heroe1) => {
+  (err) ? console.error(err) : console.log(heroe1);
+  
+  buscarHeroe(heroeId2, (err, heroe2) =>{
+    (err) ? console.error(err) : console.log(`Enviando a la misión ${ heroe2.nombre }`);
+  });
+
 });
