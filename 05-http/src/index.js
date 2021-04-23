1bbe765ddc601ec27454d49ec4556a72d035eab0
CesarArellano <raywayday@gmail.com>
2021-04-22 23:31:57 -0500

    Pro tip: Promesas en cadena

--- 05-http/src/index.js
+++ 05-http/src/index.js
@@ -1,6 +1,12 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch(jokeUrl).then(async resp => {
-  const decodedData = await resp.json();
-  console.log(decodedData.value);
-});
+// fetch(jokeUrl).then(async resp => {
+//   const decodedData = await resp.json();
+//   console.log(decodedData.value);
+// });
+
+fetch(jokeUrl)
+  .then( resp => resp.json())
+  .then( ({ id, value }) => {
+    console.log(id, value);
+  });
